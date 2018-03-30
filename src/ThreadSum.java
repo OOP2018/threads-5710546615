f14a7bb5775fca51b55473ffaa90c798732a1ac1
Visurt Anuttivong <visurt.a@ku.th>
2018-03-30 15:21:48 +0700

    update run thread using executor

--- src/ThreadSum.java
+++ src/ThreadSum.java
@@ -1,3 +1,7 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 /**
  * An application class for threads.
  * 
@@ -12,40 +16,43 @@ public class ThreadSum {
 	public static void main(String[] args) {
 		// upper limit of numbers to add/subtract to Counter
 		final int LIMIT = 10000000;
+		// number of threads
+		int nthread = 5;
 		// The counter that accumulates a total.
 		Counter counter = new Counter();
 		// Counter counter = new CounterWithLock();
 		// Counter counter = new SynchronousCounter();
 		// Counter counter = new AtomicCounter();
 
-		runThreads(counter, LIMIT);
+		runThreads(nthread, counter, LIMIT);
 	}
 
 	/**
 	 * Running the tasks and print time used.
 	 * 
+	 * @param nthread the no of each thread to run
 	 * @param counter the counter of tasks
 	 * @param limit the limit number of added number
 	 */
-	public static void runThreads(Counter counter, final int limit) {
-		// two tasks that add and subtract values using same Counter
-		AddTask addtask = new AddTask(counter, limit);
-		SubtractTask subtask = new SubtractTask(counter, limit);
-		// threads to run the tasks
-		Thread thread1 = new Thread(addtask);
-		Thread thread2 = new Thread(subtask);
+	public static void runThreads(int nthread, Counter counter, final int limit) {
+		ExecutorService executor = Executors.newFixedThreadPool(2 * nthread);
 		// start the tasks
 		System.out.println("Starting threads");
 		long startTime = System.nanoTime();
-		thread1.start();
-		thread2.start();
+		for (int k = 1; k <= nthread; k++) {
+			Runnable addtask = new AddTask(counter, limit);
+			Runnable subtask = new SubtractTask(counter, limit);
+			executor.submit(addtask);
+			executor.submit(subtask);
+		}
 		// wait for threads to finish
 		try {
-			thread1.join();
-			thread2.join();
+			executor.shutdown();
+			executor.awaitTermination(1, TimeUnit.MINUTES);
 		} catch (InterruptedException e) {
 			System.out.println("Threads interrupted");
 		}
+		// print elapsed time and counter value
 		double elapsed = 1.0E-9 * (System.nanoTime() - startTime);
 		System.out.printf("Count 1 to %,d in %.6f sec\n", limit, elapsed);
 		System.out.printf("Counter total is %d\n", counter.get());
